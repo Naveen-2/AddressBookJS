8d2cadfc2ad64dd928a747adc3badac4535e339b
Naveen-2 <naveen2k15cse@gmail.com>
2022-04-08 00:48:39 +0530

    Find number of contacts

--- AddressBook.js
+++ AddressBook.js
@@ -35,142 +35,152 @@ function addContactDetails(firstName, lastName, address, state, city, zipCode, e
     } else {
         throw 'Contact Details Are Invalid';
     }
+}
+function isFirstNameVaild(firstName){
+    const firstNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+    return firstNamePattern.test(firstName);
+}
 
-    function isFirstNameVaild(firstName){
-        const firstNamePattern = /^[A-Z][a-zA-Z]{3,}/;
-        return firstNamePattern.test(firstName);
-    }
+function isLastNameValid(lastName){
+    const lastNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+    return lastNamePattern.test(lastName);
+}
 
-    function isLastNameValid(lastName){
-        const lastNamePattern = /^[A-Z][a-zA-Z]{3,}/;
-        return lastNamePattern.test(lastName);
-    }
+function isAddressValid(address){
+    const addressPattern = /^[A-Z][a-zA-Z0-9]{3,}/;
+    return addressPattern.test(address);
+}
 
-    function isAddressValid(address){
-        const addressPattern = /^[A-Z][a-zA-Z0-9]{3,}/;
-        return addressPattern.test(address);
-    }
+function isStateValid(state){
+    const statePattern = /^[A-Z][a-zA-Z]{3,}/;
+    return statePattern.test(state);
+}
 
-    function isStateValid(state){
-        const statePattern = /^[A-Z][a-zA-Z]{3,}/;
-        return statePattern.test(state);
-    }
+function isCityValid(city){
+    const cityPattern = /^[A-Z][a-zA-Z]{3,}/;
+    return cityPattern.test(city);
+}
 
-    function isCityValid(city){
-        const cityPattern = /^[A-Z][a-zA-Z]{3,}/;
-        return cityPattern.test(city);
-    }
+function isZipCodeValid(zipCode){
+    const zipCodePattern = /^[1-9]{1}[0-9]{5}/;
+    return zipCodePattern.test(zipCode);
+}
 
-    function isZipCodeValid(zipCode){
-        const zipCodePattern = /^[1-9]{1}[0-9]{5}/;
-        return zipCodePattern.test(zipCode);
-    }
+function isEmailValid(email){
+    const emailPattern = /^[A-Za-z0-9]+(.[A-Za-z0-9]+)*@[^_\\W]+(.[^_\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).*$/;
+    return emailPattern.test(email);
+}
 
-    function isEmailValid(email){
-        const emailPattern = /^[A-Za-z0-9]+(.[A-Za-z0-9]+)*@[^_\\W]+(.[^_\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).*$/;
-        return emailPattern.test(email);
-    }
+function isPhoneNumberValid(phoneNumber){
+    const phoneNumberPattern = /^[1-9]{1}[0-9]{9}/;
+    return phoneNumberPattern.test(phoneNumber);
+}
 
-    function isPhoneNumberValid(phoneNumber){
-        const phoneNumberPattern = /^[1-9]{1}[0-9]{9}/;
-        return phoneNumberPattern.test(phoneNumber);
-    }
-
-    function editContact(firstName, changingField, newValue) {
-        if (addressBook.length == null) {
-            console.log("Address Book Empty");
-        } else {
-            addressBook.forEach(newContact => {
-                if (newContact.firstName == firstName) {
-                    switch (changingField) {
-                        case "firstName":
-                            if(isFirstNameVaild(newValue)){
-                                newContact.firstName = newValue;
-                                console.log("First Name updated successfully");
-                                return;
-                            } 
-                        case "lastName":
-                            if(isLastNameVaild(newValue)){
-                                newContact.lastName = newValue;
-                                console.log("Last Name updated successfully");
-                                return;
-                            }
-                        case "address":
-                            if(isAddressValid(newValue)){
-                                newContact.address = newValue;
-                                console.log("Address updated successfully");
-                                return;
-                            }
-                        case "state":
-                            if(isStateValid(newValue)){
-                                newContact.state = newValue;
-                                console.log("State updated successfully");
-                                return;
-                            }
-                        case "city":
-                            if(isCityValid(newValue)){
-                                newContact.city = newValue;
-                                console.log("City updated successfully");
-                                return;
-                            }
-                        case "zipCode":
-                            if(isZipCodeValid(newValue)){
-                                newContact.zipCode = newValue;
-                                console.log("Zip Code updated successfully");
-                                return;
-                            }
-                        case "email":
-                            if(isEmailValid(newValue)){
-                                newContact.email = newValue;
-                                console.log("Email updated successfully");
-                                return;
-                            }
-                        case "phoneNumber":
-                            if(isPhoneNumberValid(newValue)){
-                                newContact.phoneNumber = newValue;
-                                console.log("Phone number updated successfully");
-                                return;
-                            }
-                    }
-
-                    console.log("Invalid entry");
+function editContact(firstName, changingField, newValue) {
+    if (addressBook.length == null) {
+        console.log("Address Book Empty");
+    } else {
+        addressBook.forEach(newContact => {
+            if (newContact.firstName == firstName) {
+                switch (changingField) {
+                    case "firstName":
+                        if(isFirstNameVaild(newValue)){
+                            newContact.firstName = newValue;
+                            console.log("First Name updated successfully");
+                            return;
+                        } 
+                    case "lastName":
+                        if(isLastNameVaild(newValue)){
+                            newContact.lastName = newValue;
+                            console.log("Last Name updated successfully");
+                            return;
+                        }
+                    case "address":
+                        if(isAddressValid(newValue)){
+                            newContact.address = newValue;
+                            console.log("Address updated successfully");
+                            return;
+                        }
+                    case "state":
+                        if(isStateValid(newValue)){
+                            newContact.state = newValue;
+                            console.log("State updated successfully");
+                            return;
+                        }
+                    case "city":
+                        if(isCityValid(newValue)){
+                            newContact.city = newValue;
+                            console.log("City updated successfully");
+                            return;
+                        }
+                    case "zipCode":
+                        if(isZipCodeValid(newValue)){
+                            newContact.zipCode = newValue;
+                            console.log("Zip Code updated successfully");
+                            return;
+                        }
+                    case "email":
+                        if(isEmailValid(newValue)){
+                            newContact.email = newValue;
+                            console.log("Email updated successfully");
+                            return;
+                        }
+                    case "phoneNumber":
+                        if(isPhoneNumberValid(newValue)){
+                            newContact.phoneNumber = newValue;
+                            console.log("Phone number updated successfully");
+                            return;
+                        }
                 }
-            });
-        }
-    }
 
-    function deleteContact(first_Name) {
-        if (addressBook.length == null) {
-            console.log("Address Book Empty");
-        } else {
-            for (let i = 0; i < addressBook.length; i++) {
-                if (addressBook[i].firstName == first_Name) {
-                    addressBook.splice(i, 1);
-                    console.log("Contact Deleted Successfully");
-                }
+                console.log("Invalid entry");
+            }
+        });
+    }
+}
+
+function deleteContact(first_Name) {
+    if (addressBook.length == null) {
+        console.log("Address Book Empty");
+    } else {
+        for (let i = 0; i < addressBook.length; i++) {
+            if (addressBook[i].firstName == first_Name) {
+                addressBook.splice(i, 1);
+                console.log("Contact Deleted Successfully");
             }
         }
     }
-
-    function useFunction(option) {
-        switch (option) {
-            case "addContactDetails":
-                addContactDetails("Naveen", "Kumar", "NewRoadStreet", "VWXYZ", "TamilNadu", "626987", "contact@naveen.com", "9876543210");
-                addContactDetails("Hareesh", "Raj", "OldRoadStreet", "ZYXWV", "TamilNadu", "626145", "contact@hareesh.com", "9873216540");
-                break;
-
-            case "editContact":
-                editContact("Naveen", "city", "ABCDE");
-                break;
-
-            case "deleteContact":
-                deleteContact("Hareesh");
-                break;
-        }
-    }
-
-    useFunction("addContactDetails");
-    useFunction("editContact");
-    useFunction("deleteContact");
-    
 }
+
+function countContacts() {
+    addressBook.reduce(() => {
+        count++;
+    }, count = 0);
+    console.log("\nTotal Contacts in Address Book are: " + count + "\n");
+}
+
+function useFunction(option) {
+    switch (option) {
+        case "addContactDetails":
+            addContactDetails("Naveen", "Kumar", "NewRoadStreet", "VWXYZ", "TamilNadu", "626987", "contact@naveen.com", "9876543210");
+            addContactDetails("Hareesh", "Raj", "OldRoadStreet", "ZYXWV", "TamilNadu", "626145", "contact@hareesh.com", "9873216540");
+            break;
+
+        case "editContact":
+            editContact("Naveen", "city", "ABCDE");
+            break;
+
+        case "deleteContact":
+            deleteContact("Hareesh");
+            break;
+        case "countContacts":
+            countContacts();
+            break;
+    }
+}
+
+useFunction("addContactDetails");
+useFunction("editContact");
+useFunction("deleteContact");
+useFunction("countContacts");
+    
