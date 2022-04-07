fec545b39ece21c010ef579b6cf63e5038543566
Naveen-2 <naveen2k15cse@gmail.com>
2022-04-07 23:51:27 +0530

    Contacts can be validated and added. Invalid contacts will not be added.

---
+++ AddressBook.js
@@ -0,0 +1,61 @@
+class Contact {
+    firstName
+    lastName
+    address
+    state
+    city
+    zipCode
+    email
+    phoneNumber
+
+    constructor(firstName, lastName, address, state, city, zipCode, email, phoneNumber) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.address = address
+        this.state = state
+        this.city = city
+        this.zipCode = zipCode
+        this.email = email
+        this.phoneNumber = phoneNumber
+    }
+}
+
+function contactDetails(firstName, lastName, address, state, city, zipCode, email, phoneNumber) {
+
+    const firstNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+    let isFirstNameVaild = firstNamePattern.test(firstName);
+
+    const lastNamePattern = /^[A-Z][a-zA-Z]{3,}/;
+    let isLastNameValid = lastNamePattern.test(lastName);
+
+    const addressPattern = /^[A-Z][a-zA-Z0-9]{3,}/;
+    let isAddressValid = addressPattern.test(address);
+
+    const statePattern = /^[A-Z][a-zA-Z]{3,}/;
+    let isStateValid = statePattern.test(state);
+
+    const cityPattern = /^[A-Z][a-zA-Z]{3,}/;
+    let isCityValid = cityPattern.test(city);
+
+    const zipCodePattern = /^[1-9]{1}[0-9]{5}/;
+    let isZipCodeValid = zipCodePattern.test(zipCode);
+
+    const emailPattern = /^[A-Za-z0-9]+(.[A-Za-z0-9]+)*@[^_\\W]+(.[^_\\W]+)?(?=(.[^_\\W]{3,}$|.[a-zA-Z]{2}$)).*$/;
+    let isEmailValid = emailPattern.test(email);
+
+    const phoneNumberPattern = /^[1-9]{1}[0-9]{9}/;
+    let isPhoneNumberValid = phoneNumberPattern.test(phoneNumber);
+
+    if (isFirstNameVaild == true && isLastNameValid == true && isAddressValid == true && isStateValid == true && isCityValid == true &&
+        isZipCodeValid == true && isEmailValid == true && isPhoneNumberValid == true) {
+
+        let newContact = new Contact(firstName, lastName, address, state, city, zipCode, email, phoneNumber);
+        console.log("Contact Added Successfully");
+
+        addressBook.push(newContact);
+
+    } else {
+        throw 'Contact Details Are Invalid';
+    }
+    
+}
